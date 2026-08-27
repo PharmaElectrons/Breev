7260eebc770cd1a6a6c6ff47728976466f4051fc
Mahmoud Ahmed <mahmoud21-00907@student.eelu.edu.eg>
2026-08-27 13:43:00 +0200

    fix(recovery): register migration journal entry, resolve module init hook, and update health test schema version

--- apps/local-api/src/recovery/recovery.module.ts
+++ apps/local-api/src/recovery/recovery.module.ts
@@ -1,16 +1,11 @@
-import { Module, type OnModuleInit } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 
 import { DurableJobsService } from "../durable-jobs/durable-jobs.service.js";
 import { LocalDatabaseService } from "../local-database.service.js";
 import { RecoveryCoordinatorService } from "./recovery-coordinator.service.js";
 import { RecoveryJobService } from "./recovery-job.service.js";
 import { RecoveryController } from "./recovery.controller.js";
-import {
-  DeviceIdentityVerificationHook,
-  LicenceTimeVerificationHook,
-  MainDeviceSecurityVerificationHook,
-  RestoreQuarantineService,
-} from "./restore-quarantine.service.js";
+import { RestoreQuarantineService } from "./restore-quarantine.service.js";
 
 @Module({
   controllers: [RecoveryController],
@@ -27,16 +22,4 @@ import {
     RecoveryJobService,
   ],
 })
-export class RecoveryModule implements OnModuleInit {
-  public constructor(
-    private readonly quarantineService: RestoreQuarantineService,
-  ) {}
-
-  public onModuleInit(): void {
-    this.quarantineService.registerHook(
-      new MainDeviceSecurityVerificationHook(),
-    );
-    this.quarantineService.registerHook(new DeviceIdentityVerificationHook());
-    this.quarantineService.registerHook(new LicenceTimeVerificationHook());
-  }
-}
+export class RecoveryModule {}
